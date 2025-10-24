d34d7c4d6feb25f118e5f98d60685fd34ff62996
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-25 03:37:02 +0600

    Added the schema validation middleware

---
+++ src/middleware/validationHandler.js
@@ -0,0 +1,9 @@
+export const validate = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if(error) {
+        return res.status(400).message({
+            message: error.details[0].message
+        });
+    }
+    next();
+}
