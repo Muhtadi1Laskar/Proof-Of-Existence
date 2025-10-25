bdc38be6fd5ad2f0020e0489a8bdf3dd71798925
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-25 17:10:45 +0600

    Changed file name

--- src/controllers/proof.controller.js
+++ src/controllers/proof.controller.js
@@ -1,4 +1,4 @@
-import { createProof } from "../services/createProof.service.js";
+import { createProof } from "../services/proof.service.js";
 import { generateHash } from "../services/hash.service.js";
 import { errorResponse, successResponse } from "../utils/response.js"
 
