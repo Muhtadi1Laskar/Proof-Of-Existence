270eff4a43c7ba9da56623fda3e948d84e5ac24f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-26 15:24:55 +0600

    Added service to retrive all proof by owner name

---
+++ src/services/retrive.proof.service.js
@@ -0,0 +1,15 @@
+import proofModel from "../models/proof.model.js";
+
+export const retriveByOwner = async (owner) => {
+    if (!owner) {
+        throw new Error("Give a valid owner name");
+    }
+
+    if(owner.length === 0) {
+        throw new Error("Owner cannot be empty");
+    }
+
+    const proof = proofModel.find({ owner });
+
+    return proof;
+}
